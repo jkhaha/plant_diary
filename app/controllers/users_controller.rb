84d243da88299416641b307c0c4c4af25f918474
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-10-01 17:05:57 -0400

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
 
-  def index
-    @user = User.new
-    if @user.valid?
-      @user.save
-      redirect_to user_path(@user)
-    else
-
-    end
+  # def index
+  #   @user = User.new
+  #   if @user.valid?
+  #     @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #
+  #   end
 
     def show
       @user = User.find(params[:id])
       @plants = @user.plants
     end
-  end
 
   private
 
