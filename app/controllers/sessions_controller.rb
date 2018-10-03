08714209a24ecc8eaa23e52ed8a21f5c2165da7c
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-10-03 16:20:50 -0400

    Added bootstrap, css styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  def index
+  end
 
   def new
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "sessions#index"
 
   resources :users, only: [:show, :new, :create]
-
 
   resources :plants, only: [:show, :new, :create, :edit, :update, :destroy]
 
   resources :comments, only: [:new, :create]
 
+  resources :tasks, only: [:new, :create, :destroy]
+
   resources :sessions, only: [:new, :create]
+
   delete "/sessions", to: "sessions#destroy"
-
-  resources :tasks, only: [:new, :create, :edit, :update, :destroy]
-
 end
